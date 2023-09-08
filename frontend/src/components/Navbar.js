31c3913692b708dc8c0c1ad986716652f99c918d
joseph0017 <josephogiku96@gmail.com>
2023-09-08 11:51:30 +0100

    responsiveness

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
   return (
     <nav className="w-full bg-white shadow">
-    <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+    <div className="justify-between px-4 mx-auto lg:max-w-7xl lg:items-center lg:flex lg:px-8">
         <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
             <Link to="/" className="pl-8 sm:text-base italic font-medium cursor-pointer ">
                 LuxyPalace
             </Link>
-                <div className="md:hidden">
+                <div className="lg:hidden">
                     <button
                         className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
                         onClick={() => setNavbar(!navbar)}
@@ -53,11 +53,11 @@ const Navbar = () => {
         </div>
         <div>
             <div
-                className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
                     navbar ? "block" : "hidden"
                 }`}
             >
-                <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 cursor-pointer">
+                <ul className="items-center justify-center space-y-8 lg:flex lg:space-x-6 lg:space-y-0 cursor-pointer">
                     <li className="text-gray-600 ">
                     <Link className="border-b-2 border-transparent hover:text-orange-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-orange-300 mx-1.5 sm:mx-6 text-base" to="/">
                       Home
