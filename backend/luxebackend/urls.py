57e780f4aeea9133411479d7d060c83651f17fbc
joseph0017 <josephogiku96@gmail.com>
2023-09-11 11:27:19 +0100

    added requirements.txt file

--- backend/luxebackend/urls.py
+++ backend/luxebackend/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import re_path
+from django.contrib import admin
 
 urlpatterns = [
+    re_path('admin/', admin.site.urls),
     re_path('login', views.login),
     re_path('signup', views.signup),
     re_path('test_token', views.test_token),
