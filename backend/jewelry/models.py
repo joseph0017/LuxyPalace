6ff85d47fb7d35a99fd5661e6bb9a25f40ece20a
Amarajah <104323769+Amarajah@users.noreply.github.com>
2023-09-18 20:00:22 +0100

    modified models.py

--- backend/jewelry/models.py
+++ backend/jewelry/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Client(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.username
-
 class Jewelry(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -17,7 +10,6 @@ class Jewelry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    owner = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='jewelries')
 
     def __str__(self):
         return self.name
